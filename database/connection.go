b7fc8f5ea21f9472c7399a4808ad98e65d1502af
hasanovdev <hasanovdev@mail.ru>
2023-02-16 13:18:55 +0500

    first commit

---
+++ database/connection.go
@@ -0,0 +1,22 @@
+package database
+
+import (
+	"jwt/models"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	connection, err := gorm.Open(mysql.Open("root:Hasanov@0301@/jwt_auth"), &gorm.Config{})
+
+	if err != nil {
+		panic("could not connect to the database")
+	}
+
+	DB = connection
+
+	connection.AutoMigrate(&models.User{})
+}
